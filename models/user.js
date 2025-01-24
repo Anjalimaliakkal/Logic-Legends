5debd50e71d3ad0bc3df5afd98b0285c4a81022e
Anjalimaliakkal <147378299+Anjalimaliakkal@users.noreply.github.com>
2025-01-24 19:38:46 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -3,8 +3,6 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    cupsSaved: { type: Number, default: 0 },
-    points: { type: Number, default: 0 },
   });
 const userModel=mongoose.model("userdata",userSchema)
 module.exports=userModel
